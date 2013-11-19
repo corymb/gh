30257cc878e84eaaeeeb5c5a22f76514d056fe43
David Bush <davidbush@gmail.com>
2013-11-19 21:33:56 +0000

    Fixed score saving issue, fixed initial score default bug and added AJAX voting.

--- app/models.py
+++ app/models.py
@@ -6,9 +6,18 @@ from app import db
 class Confession(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     confession = db.Column(db.Text())
-    score = db.Integer(default=0)
+    score = db.Column(db.Integer(default=1))
     uploaded_time = db.Column(db.DateTime, default=datetime.now)
     approved_time = db.Column(db.DateTime, default=None)
+
+    def __init__(self, *args, **kwargs):
+        """
+        SQLAlchemy only populates default values with INSERT and UPDATE, so you
+        have to manually set the starting score:
+        """
+        if 'score' not in kwargs:
+             kwargs['score'] = 1
+        super(Confession, self).__init__(**kwargs)
 
     def approve(self):
         self.approved_time = datetime.now()
@@ -17,6 +26,14 @@ class Confession(db.Model):
     def store_confession(self):
         db.session.add(self)
         db.session.commit()
+
+    def upvote(self):
+        self.score += 1
+        self.store_confession()
+
+    def downvote(self):
+        self.score -= 1
+        self.store_confession()
 
     def __repr__(self):
         return '<Confession: %r>' % self.confession
--- views.py
+++ views.py
@@ -5,18 +5,33 @@ from app.models import Confession
 
 
 @app.route('/')
-def show_entries():
+def show_confessions():
     confessions = Confession.query.filter(Confession.approved_time !=
             None).order_by(Confession.approved_time.desc())
     return render_template('show_entries.html', confessions=confessions)
 
+
+@app.route('/<confession_id>', methods=['GET', 'POST'])
+def show_confession(confession_id):
+    confession = Confession.query.get(confession_id)
+    if request.method == 'POST':
+        upvote = True if request.form.get('upvote') == '1' else False
+        if upvote:
+            confession.upvote()
+        else:
+            confession.downvote()
+        return u'%s' % confession.score
+
+    # Template re-use requires an iterable:
+    confession = confession,
+    return render_template('show_entries.html', confessions=confession)
 
 @app.route('/add/', methods=['POST'])
 def add_entry():
     confession = Confession(confession=request.form['confession'])
     confession.store_confession()
     flash('Your confession has been posted and is pending approval')
-    return redirect(url_for('show_entries'))
+    return redirect(url_for('show_confessions'))
 
 
 @app.route('/downvote/')
